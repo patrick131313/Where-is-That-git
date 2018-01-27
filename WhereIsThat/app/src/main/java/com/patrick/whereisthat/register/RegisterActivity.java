b347e59a38fe03d25a6eddaba6ede2c730b15f1f
Suciu Patrick <suciu.patrick13@gmail.com>
2018-01-27 14:48:13 +0200

    User exist finished

--- WhereIsThat/app/src/main/java/com/patrick/whereisthat/register/RegisterActivity.java
+++ WhereIsThat/app/src/main/java/com/patrick/whereisthat/register/RegisterActivity.java
@@ -34,6 +34,7 @@ public class RegisterActivity extends AppCompatActivity {
 
     private FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private boolean mExist;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,68 +49,97 @@ public class RegisterActivity extends AppCompatActivity {
         mCreateAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final String email=mEmail.getText().toString();
-                final String user=mUsername.getText().toString();
-                final String password=mPassword.getText().toString();
-                Log.d("Email:",email.toString());
-                Log.d("Password:",password.toString());
-                checkIfUserExists(user);
-              mFirebaseAuth.createUserWithEmailAndPassword(email,password).
-                        addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                final String email = mEmail.getText().toString();
+                final String user = mUsername.getText().toString();
+                final String password = mPassword.getText().toString();
+                Log.d("Email:", email.toString());
+                Log.d("Password:", password.toString());
+                //checkIfUserExists(user);
+                Log.d("Boolean:",String.valueOf(mExist));
+                checkIfUserExists(email,user,password);
+            }
+        });
+
+
+
+
+    }
+   int checkIfUserExists(final String email,final String user,final String password)
+    {
+        final int[] flag = {0};
+        Log.d("Flag-start", String.valueOf(flag[0]));
+       DatabaseReference myRef=FirebaseDatabase.getInstance().getReference();
+                Query query=myRef.child("users").orderByChild("user").equalTo(user);
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()) {
-                            Toast.makeText(getApplication(), "Registration successful", Toast.LENGTH_LONG).show();
-                            String userID = mFirebaseAuth.getCurrentUser().getUid();
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.exists()) {
+                            Log.d("Query", "User exits");
+                            toastExits();
+                            flag[0]=1;
 
-                           DatabaseReference userDb= FirebaseDatabase.getInstance().getReference().child("users").child(userID);
-                            Map info=new HashMap<>();
-                            info.put("user",user);
-                            info.put("email",email);
-                            for(int i=1;i<12;i++)
-                                info.put("level"+String.valueOf(i),0);
-                            info.put("sprint_mode",0);
-                            userDb.updateChildren(info);
+                        }
+                        else {
+                            Log.d("Query", "You cand add this user");
+                            toastSuccesful();
+                            mFirebaseAuth.createUserWithEmailAndPassword(email, password).
+                                    addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<AuthResult> task) {
+                                            if (task.isSuccessful()) {
+                                                //
+                                                String userID = mFirebaseAuth.getCurrentUser().getUid();
 
+                                                DatabaseReference userDb = FirebaseDatabase.getInstance().getReference().child("users").child(userID);
+                                                Map info = new HashMap<>();
+                                                info.put("user", user);
+                                                info.put("email", email);
+                                                for (int i = 1; i < 12; i++)
+                                                    info.put("level" + String.valueOf(i), 0);
+                                                info.put("sprint_mode", 0);
+                                                userDb.updateChildren(info);
+
+
+                                            } else
+                                                Toast.makeText(getApplication(), "Error", Toast.LENGTH_LONG).show();
+
+                                        }
+                                    }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+
+                                }
+                            });
 
 
                         }
-                        else
-                            Toast.makeText(getApplication(),"Error",Toast.LENGTH_LONG).show();
-
                     }
-                }).addOnFailureListener(new OnFailureListener() {
+
                     @Override
-                    public void onFailure(@NonNull Exception e) {
+                    public void onCancelled(DatabaseError databaseError) {
 
                     }
                 });
-            }
-        });
+        Log.d("Flag", String.valueOf(flag[0]));
 
+
+        if(flag[0]==1)
+            return 1;
+        else
+            return 0;
     }
-    boolean checkIfUserExists(String user)
+    public void toastSuccesful()
     {
-        DatabaseReference myRef=FirebaseDatabase.getInstance().getReference();
-        Query query=myRef.child("users").orderByChild("user").equalTo(user);
-        query.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                if(dataSnapshot.exists())
-                    Log.d("Query","User exits");
-                else
-                    Log.d("Query","You cand add this user");
-            }
+        Toast.makeText(getApplication(), "Registration successful", Toast.LENGTH_LONG).show();
+        mExist=false;
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-
-        return false;
     }
+    public void toastExits()
+    {
+        Toast.makeText(this,"This user already exists",Toast.LENGTH_LONG).show();
+        mExist=true;
 
+    }
 
 }
 
