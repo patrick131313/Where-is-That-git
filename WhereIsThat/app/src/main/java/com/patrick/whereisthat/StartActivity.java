8dc929bcc953d397774fbef95a624bd0f7dec538
Suciu Patrick <suciu.patrick13@gmail.com>
2018-06-18 17:26:36 +0300

    Check internet connection completed

--- WhereIsThat/app/src/main/java/com/patrick/whereisthat/StartActivity.java
+++ WhereIsThat/app/src/main/java/com/patrick/whereisthat/StartActivity.java
@@ -1,6 +1,8 @@
 package com.patrick.whereisthat;
 
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentActivity;
@@ -13,20 +15,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MapStyleOptions;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -34,14 +27,15 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-import com.patrick.whereisthat.data.Scores;
 import com.patrick.whereisthat.login.LoginActivity;
 import com.patrick.whereisthat.scores.ScoresActivity;
 import com.patrick.whereisthat.selectlevel.SelectLevelActivity;
 import com.patrick.whereisthat.settings.SettingsActivity;
 import com.patrick.whereisthat.sprint.SprintActivity;
 
-import java.util.List;
+import java.net.InetAddress;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class StartActivity extends FragmentActivity {
@@ -55,15 +49,18 @@ public class StartActivity extends FragmentActivity {
     private ProgressBar mProgress;
     private Button mSprint;
     private boolean mInit;
+    private boolean mInternet;
+    private Timer timer;
+    private TimerTask timerTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
         mFirebaseAuth = FirebaseAuth.getInstance();
         setUpNavDrawer();
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
-
         mProgress=findViewById(R.id.progress_user);
         mSprint=findViewById(R.id.button_sprint_mode);
         textView=findViewById(R.id.textView_user);
@@ -71,93 +68,136 @@ public class StartActivity extends FragmentActivity {
         mSprint.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(!mUser.equals("")) {
-                    Intent intent = new Intent(getApplicationContext(), SprintActivity.class);
-                    startActivity(intent);
+                if(mInternet) {
+                    if (!mUser.equals("")) {
+                        timer.cancel();
+                        timerTask.cancel();
+                        Intent intent = new Intent(getApplicationContext(), SprintActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(getApplicationContext(), "You must be logged in to play, swipe left to login", Toast.LENGTH_LONG).show();
+                    }
                 }
                 else
                 {
-                    Toast.makeText(getApplicationContext(),"You must be logged in to play, swipe left to login",Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "Check your internet connection", Toast.LENGTH_LONG).show();
                 }
             }
         });
-        if (mFirebaseAuth.getCurrentUser() != null) {
-            getUsername(mFirebaseAuth.getUid());
-            mInit=true;
-            Log.d("Login", mFirebaseAuth.getUid());
-        }
-        else
-        {
-            mProgress.setVisibility(View.INVISIBLE);
-            mInit=false;
-            changeMenuToLogout();
-            deleteUsername();
-        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        timer.cancel();
+        timerTask.cancel();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        newTimerTask();
+        timer=new Timer();
+        timer.schedule(timerTask, 0,10000);
+    }
+
+    public void newTimerTask()
+    {
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                //CALL YOUR ASSYNC TASK HERE.
+                new CheckInternetTask().execute();
+            }
+        };
     }
     public void startGameClick(View view){
 
-        if(!mUser.equals("")) {
-            Intent intent = new Intent(getApplication(), SelectLevelActivity.class);
-            startActivity(intent);
+        if(mInternet) {
+            if (!mUser.equals("")) {
+                timer.cancel();
+                timerTask.cancel();
+                Intent intent = new Intent(getApplication(), SelectLevelActivity.class);
+                startActivity(intent);
+            } else {
+                Toast.makeText(getApplicationContext(), "You must be logged in to play, swipe left to login", Toast.LENGTH_LONG).show();
+            }
         }
         else
         {
-            Toast.makeText(getApplicationContext(),"You must be logged in to play, swipe left to login",Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Check your internet connection", Toast.LENGTH_LONG).show();
         }
     }
     public void setUpNavDrawer()
     {
-
         NavigationView mNavigationView=findViewById(R.id.nav_view);
-
         if(mNavigationView!=null)
             setUpNavListener(mNavigationView);
     }
 
     public void setUpNavListener(NavigationView navigationView)
     {
-
         navigationView.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
                     @Override
                     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                         switch (item.getItemId()) {
                             case R.id.leaderboard_navigation_menu_item:
-                                Intent intent2=new Intent(getApplicationContext(), ScoresActivity.class);
-                                intent2.putExtra(EXTRA_USERNAME,mUser);
-                                startActivity(intent2);
+                                if(mInternet) {
+                                    timer.cancel();
+                                    timerTask.cancel();
+                                    Intent intent2 = new Intent(getApplicationContext(), ScoresActivity.class);
+                                    intent2.putExtra(EXTRA_USERNAME, mUser);
+                                    startActivity(intent2);
+                                }
+                                else
+                                    Toast.makeText(getApplicationContext(), "Check your internet connection", Toast.LENGTH_LONG).show();
                                 break;
                             case R.id.invite_navigation_menu_item:
-                                Intent sendIntent = new Intent();
-                                String Title = "Share";
-                                Intent chooser = Intent.createChooser(sendIntent, Title);
-                                sendIntent.setAction(Intent.ACTION_SEND);
-                                sendIntent.putExtra(Intent.EXTRA_SUBJECT," 'Where is this?' - game");
-                                sendIntent.putExtra(Intent.EXTRA_TEXT, "Hi, you must try this game! #whereisthis"+System.getProperty("line.separator")+"www.whereisthisapp.com");
-                                sendIntent.setType("text/plain");
-                                if (sendIntent.resolveActivity(getPackageManager()) != null)
-                                    startActivity(chooser);
+                                if(mInternet) {
+                                    timer.cancel();
+                                    timerTask.cancel();
+                                    Intent sendIntent = new Intent();
+                                    String Title = "Share";
+                                    Intent chooser = Intent.createChooser(sendIntent, Title);
+                                    sendIntent.setAction(Intent.ACTION_SEND);
+                                    sendIntent.putExtra(Intent.EXTRA_SUBJECT, " 'Where is this?' - game");
+                                    sendIntent.putExtra(Intent.EXTRA_TEXT, "Hi, you must try this game! #whereisthis" + System.getProperty("line.separator") + "www.whereisthisapp.com");
+                                    sendIntent.setType("text/plain");
+                                    if (sendIntent.resolveActivity(getPackageManager()) != null)
+                                        startActivity(chooser);
+                                }
+                                else
+                                {
+                                    Toast.makeText(getApplicationContext(), "Check your internet connection", Toast.LENGTH_LONG).show();
+                                }
                                 break;
                             case R.id.settings_navigation_menu_item:
+                                timer.cancel();
+                                timerTask.cancel();
                                 Intent intent3=new Intent(getApplicationContext(),SettingsActivity.class);
                                 startActivity(intent3);
                                 break;
                             case R.id.logout_navigation_menu_item:
-                                String title=item.getTitle().toString();
+                                if(mInternet) {
+                                    String title = item.getTitle().toString();
+                                    if (title.equals("Logout")) {
+                                        FirebaseAuth.getInstance().signOut();
+                                        logoutToast();
+                                        changeMenuToLogout();
+                                        deleteUsername();
 
-                                if(title.equals("Logout"))
-                                {
-                                    FirebaseAuth.getInstance().signOut();
-                                    logoutToast();
-                                    changeMenuToLogout();
-                                    deleteUsername();
-
+                                    } else {
+                                        Log.d("Logout", "Logout clicked");
+                                        Intent intent = new Intent(getApplication(), LoginActivity.class);
+                                        startActivity(intent);
+                                        break;
+                                    }
                                 }
-                                else {
-                                    Log.d("Logout", "Logout clicked");
-                                    Intent intent = new Intent(getApplication(), LoginActivity.class);
-                                    startActivity(intent);
-                                    break;
+                             else
+                                {
+                                    Toast.makeText(getApplicationContext(), "Check your internet connection", Toast.LENGTH_LONG).show();
                                 }
 
 
@@ -208,17 +248,67 @@ public class StartActivity extends FragmentActivity {
 
     public void changeMenuToLogout()
     {
-        NavigationView mNavigationView=findViewById(R.id.nav_view);
-        Menu menu=mNavigationView.getMenu();
-        MenuItem item=menu.findItem(R.id.logout_navigation_menu_item);
-        item.setTitle("Login");
-        item.setIcon(ContextCompat.getDrawable(this,R.drawable.ic_login_24dp));
-        mUser="";
+            NavigationView mNavigationView = findViewById(R.id.nav_view);
+            Menu menu = mNavigationView.getMenu();
+            MenuItem item = menu.findItem(R.id.logout_navigation_menu_item);
+            item.setTitle("Login");
+            item.setIcon(ContextCompat.getDrawable(this, R.drawable.ic_login_24dp));
+            mUser = "";
     }
     public void logoutToast()
     {
         Toast.makeText(this,"You are logged out",Toast.LENGTH_LONG).show();
     }
+
+    public boolean isInternetAvailable() {
+        try {
+            InetAddress ipAddr = InetAddress.getByName("google.com");
+            //You can replace it with your name
+            Log.i("CheckConnection", ipAddr.toString());
+            return !ipAddr.equals("");
+
+        } catch (Exception e) {
+            Log.i("CheckConnection", e.toString());
+            return false;
+        }
+    }
+
+    class CheckInternetTask extends AsyncTask<Void, Void, Boolean>
+    {
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            return isInternetAvailable();
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            if(aBoolean) {
+                mInternet=true;
+                if (mFirebaseAuth.getCurrentUser() != null) {
+
+                    getUsername(mFirebaseAuth.getUid());
+
+                    mInit = true;
+                    Log.d("Login", mFirebaseAuth.getUid());
+                } else {
+                    mProgress.setVisibility(View.INVISIBLE);
+                    mInit = false;
+                    changeMenuToLogout();
+                    deleteUsername();
+                }
+            }
+            else
+            {
+                    mInternet=false;
+                    mProgress.setVisibility(View.INVISIBLE);
+                    textView.setText("No internet connection");
+                    textView.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+
 
 
 
